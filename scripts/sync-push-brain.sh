dc00732e695892678c8d29b4020c187eb9523045
Bury Huang <buryhuang@gmail.com>
2026-03-22 23:53:36 -0700

    update sync script

--- scripts/sync-push-brain.sh
+++ scripts/sync-push-brain.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-# Push local brain files to S3
+# Push local brain and users files to S3
 #
-# Source: ~/company-os/peakmojo/brain/
-# Dest:   s3://peakmojo-company-os/peakmojo/brain/
+# Source: ~/company-os/peakmojo/brain/ and ~/company-os/peakmojo/users/
+# Dest:   s3://peakmojo-company-os/peakmojo/brain/ and s3://peakmojo-company-os/peakmojo/users/
 
 set -euo pipefail
 
 BUCKET="peakmojo-company-os"
 LOCAL_BASE="${HOME}/company-os/peakmojo"
 
-echo "[$(date -Iseconds)] sync-push-brain: starting"
+echo "[$(date -Iseconds)] sync-push: starting"
 
+# Push brain
 aws s3 sync \
     "${LOCAL_BASE}/brain/" \
     "s3://${BUCKET}/peakmojo/brain/" \
     --size-only \
     --quiet
 
-echo "[$(date -Iseconds)] sync-push-brain: done"
+# Push users
+aws s3 sync \
+    "${LOCAL_BASE}/users/" \
+    "s3://${BUCKET}/peakmojo/users/" \
+    --size-only \
+    --quiet
+
+echo "[$(date -Iseconds)] sync-push: done"
